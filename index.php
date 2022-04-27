c7b1991f49743495f2ec640f1d3ee0963c4c5f00
pedroSLA <104389611+pedroSLA@users.noreply.github.com>
2022-04-27 08:36:52 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<script type="style.css"></script>
+</head>
+<body>
+
+<h1>Olá mundo</h1>
+<h2>Meu primeiro tema</h2>
+<h3>Pedro dos Reis Sanches de Souza</h3>
+
+</body>
+</html>
